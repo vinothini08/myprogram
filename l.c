6596e861751fdbb9e0126000dca345e3111bb9a7
vinothini08 <30343344+vinothini08@users.noreply.github.com>
2017-07-23 13:56:35 +0530

    Update and rename leap.c to l.c

--- l.c
+++ l.c
@@ -5,6 +5,14 @@ int n;
 printf("enter the year\n");
 scanf("%d",&n);
 if(n%4==0)
+{
+  if(n%100==0)
+  {
+    if(n%400==0)
+      printf("%d is leap year.",n);
+    else 
+      printf("%d,is not leap year.",n);
+  }
 printf("%d is leap year.",n);
 else
 printf("%d is not leap year.",n);
