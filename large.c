124edafd53bffb6253bcf10722554b9a2e8b7915
vinothini08 <30343344+vinothini08@users.noreply.github.com>
2017-07-23 14:43:09 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+prinntf("enter three different number:\n");
+scanf("%d %d %d",&x,&y,&z);
+{
+if(x>=y&&x>=y)
+printf("x is big");
+if(y>=x&&y>=z)
+printf("y is big");
+if(z>=x&&z>=y)
+printf("z is big);
+}
+return 0;
+}
