1c7f725841abc4ddfa7b0690f86034ab7b4a1a89
vinothini08 <30343344+vinothini08@users.noreply.github.com>
2017-07-24 10:25:39 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,9 +3,9 @@ int main()
 {
 int n,i,flag=0;
 printf("enter a possitive number:");
-scanf("%d",&n0;
-for(i=2;i<=n/2;++i0
-if(n%i==o)
+scanf("%d",&n);
+for(i=2;i<=n/2;++i)
+if(n%i==0)
 {
 flag=1;
 break;
